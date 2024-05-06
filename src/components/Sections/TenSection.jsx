c88beb178a79c68f49275b0c76b6f936ca7fbf60
nacho337 <ignaciorossatti9@gmail.com>
2024-05-06 09:45:49 -0300

    boton

--- src/components/Sections/TenSection.jsx
+++ src/components/Sections/TenSection.jsx
@@ -22,12 +22,14 @@ function TenSection() {
           </h2>
           <p className="text-black text-xs mt-7">{data.sections.twelve.subtitule}</p>
           <div>
-          <div>
-          <input type="text" className="text-white bg-black rounded-md text-xl" placeholder="Enter your work email" />
-         </div>
       <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full mt-10 text-xs">
         Get in touch
+        <a href="mailto:correo@example.com"></a>
       </button>
+      <div className="text-xl mt-7 text-center">
+      <p></p>
+      
+    </div>
          </div>
           <h2 className="text-[#080808] sm:mb-60"></h2>
           <img
